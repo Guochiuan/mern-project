fbb6a78e04b805ded0ed7a5bc7ecb7ad4da9e514
Guochiuan <gwang@gatech.edu>
2025-02-01 17:44:55 +0800

    set upload storage to be Cloudinary Cloud

---
+++ backend/routes/cloudinaryRoutes.js
@@ -0,0 +1,60 @@
+import path from "path";
+import express from "express";
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { v2 as cloudinary } from "cloudinary";
+
+const router = express.Router();
+
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Set up Cloudinary storage
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "uploads",
+    // allowedFormats: ["jpg", "png", "jpeg", "gif", "bmp"],
+    // format: async (req, file) => "png", // supports promises as well
+    public_id: (req, file) => `${file.fieldname}-${Date.now()}`,
+    // `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`,
+  },
+});
+
+// // File filter to allow only PNG and JPEG files
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/gif" ||
+    file.mimetype === "image/bmp"
+  ) {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid file type. Only image files are allowed."), false);
+  }
+};
+
+// const upload = multer({ storage, fileFilter });
+const upload = multer({ storage, fileFilter });
+const uploadSingleImage = upload.single("image");
+
+router.post("/", (req, res) => {
+  uploadSingleImage(req, res, function (err) {
+    if (err) {
+      return res.status(400).send({ message: err.message });
+    }
+
+    res.status(200).send({
+      message: "Image uploaded successfully",
+      image: req.file.path,
+    });
+  });
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-import uploadRoutes from "./routes/uploadRoutes.js";
+// import uploadRoutes from "./routes/uploadRoutes.js";
+import cloudinaryRoutes from "./routes/cloudinaryRoutes.js";
+// import cloudinaryMulterRoutes from "./routes/cloudinaryMulterRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
@@ -23,7 +25,10 @@ app.use(cookieParser());
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
-app.use("/api/upload", uploadRoutes);
+// app.use("/api/upload", uploadRoutes);
+// we can use local storage instead of cloudinary cloud storage
+app.use("/api/upload", cloudinaryRoutes);
+// app.use("/api/upload", cloudinaryMulterRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
@@ -41,7 +46,7 @@ if (process.env.NODE_ENV === "production") {
   );
 } else {
   const __dirname = path.resolve();
-  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+  // app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
   app.get("/", (req, res) => {
     res.send("API is running....");
   });
